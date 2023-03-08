3ea57a3773b026af47b65d1bf264722289eb182e
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-03-08 03:01:44 -0300

    Adicionar arquivos de projeto.

---
+++ Blog.EscolaDOTNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blog.EscolaDOTNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EscolaDOTNET.Data/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EscolaDOTNET.Data.Data;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
